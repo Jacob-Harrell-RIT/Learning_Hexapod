cbb56e90d0213b7d0fc8da29f9456a42909a717a
Jacob-Harrell-RIT <35815655+Jacob-Harrell-RIT@users.noreply.github.com>
2018-04-19 17:29:53 +0000

    6 reads, exit condition, removed threading

--- arduino_serialToPi_interface.py
+++ arduino_serialToPi_interface.py
@@ -1,22 +1,28 @@
 from __future__ import print_function
 import threading
 from time import sleep
+import sys
+import os
 import serial
 #TODO: add serial for lynxmotion ssc32
 
-ser=serial.Serial('/dev/ttyACM1', 115200)
-con_1=False
-
-def mega_get_state(ser):
+def mega_get_state(ser,con_1):
     while not con_1:
         con1=True
         while True:
-             if (ser.inWaiting()>6): #get a whole line
-             #TODO expand to arbitrary num of reads, deal with sending state data to learning algorithm
-                             d1=ser.read(ser.inWaiting())
+             if (ser.inWaiting()>36): #get a whole line
+                #TODO deal with sending state data to learning algorithm
+                d1=ser.read(ser.inWaiting())
                 print(d1,end='')
-           
-           
+                
+if __name__ == '__main__':
+    try:                 
+        ser=serial.Serial('/dev/ttyACM1', 9600)
+        conn=False
+        mega_get_state(ser,conn)
 
-thread=threading.Thread(target=mega_get_state, args=(ser,))
-thread.start()
+    except KeyboardInterrupt:
+        try:          
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
