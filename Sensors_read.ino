5317c59ada697963ba6d9fa6a08e71a78a843c98
Jacob-Harrell-RIT <35815655+Jacob-Harrell-RIT@users.noreply.github.com>
2018-04-22 15:18:35 +0000

    Create Sensors_read.ino

---
+++ Sensors_read.ino
@@ -0,0 +1,61 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM303.h>
+
+volatile int accelRDY=0;
+Adafruit_LSM303 lsm;
+const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
+int sensorValue = 0;        // value read from the pot
+int n=0;
+
+void setup(void)
+{
+  Serial.begin(115200);
+  pinMode(2,INPUT);
+  pinMode(13,OUTPUT);
+  digitalWrite(13,0);
+  /* Initialise the sensors */
+  if(!lsm.begin())
+  {
+    /* There was a problem detecting the ADXL345 ... check your connections */
+    Serial.println("no LSM303 detected");
+    while(1){
+      digitalWrite(13,1);
+      delay(100);
+      digitalWrite(13,0);
+      delay(100);
+    }
+  }
+  attachInterrupt(0,LSM_RDY,FALLING);
+}
+
+void loop(void)
+{
+  /* Get a new sensor event */
+  if(accelRDY==1){
+    lsm.read();
+    /* Display the results (acceleration is measured in m/s^2) */
+    Serial.print("Accel X: "); Serial.print((int)lsm.accelData.x); Serial.print(" ");
+    Serial.print("Y: "); Serial.print((int)lsm.accelData.y);       Serial.print(" ");
+    Serial.print("Z: "); Serial.print((int)lsm.accelData.z);     Serial.print(" ");
+    Serial.print("Mag X: "); Serial.print((int)lsm.magData.x);     Serial.print(" ");
+    Serial.print("Y: "); Serial.print((int)lsm.magData.y);         Serial.print(" ");
+    Serial.print("Z: "); Serial.println((int)lsm.magData.z);       Serial.print(" ");
+    // read the analog joint angles:
+    for(n=0;n<=5;n++){
+      sensorValue = analogRead(analogInPin+n);            
+      Serial.print("s" );  
+      Serial.print(n);
+      Serial.print(':');
+      Serial.print(sensorValue);   
+      Serial.print(",");
+    }
+    digitalWrite(13,0);
+    accelRDY=0;
+  }
+}
+
+void LSM_RDY(void){
+  accelRDY=1;
+  digitalWrite(13,1);
+ }
