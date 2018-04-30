aad080628f4464e3215d093466fea507c33675c4
Jacob-Harrell-RIT <35815655+Jacob-Harrell-RIT@users.noreply.github.com>
2018-04-30 16:50:18 -0400

    add sleep, make global

--- hex_sense_example.py
+++ hex_sense_example.py
@@ -2,25 +2,26 @@ from __future__ import print_function
 import Hexapod_Sense
 import sys
 import os
+from time import sleep
+mega=Hexapod_Sense.hexSense('/dev/ttyACM0',115200)
 
 if __name__ == '__main__':
     try:
-        mega=Hexapod_Sense.hexSense('/dev/ttyACM0',115200)
+        
         #mega.port.close()
         #mega=Hexapod_Sense.hexSense('/dev/ttyACM0',115200)
         while True:
             
             if mega.rx_state():
                 print(mega.state_array)
-            #print(mega.state_string, end='')
-            else:
-                print('-')
-            
+            sleep(0.015)
+
     except KeyboardInterrupt:
         try:
             mega.__del__()#close the port
             sys.exit(0)
         except SystemExit:
             os._exit(0)
-
-
+    finally:
+            print('err');
+            mega.__del__()
